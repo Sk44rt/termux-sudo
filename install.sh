697ee7d1665029e4996d51bc88a04c0bb7e589f0
DaZy4 <57320865+DaZy4@users.noreply.github.com>
2022-08-12 14:02:15 +0300

    Install script update.

--- install.sh
+++ install.sh
@@ -7,5 +7,6 @@ pkg install ncurses-utils
 wget https://raw.githubusercontent.com/DaZy4/termux-sudo/main/sudo
 mv sudo /data/data/com.termux/files/usr/bin
 chmod 700 /data/data/com.termux/files/usr/bin
+chmod +x /data/data/com.termux/files/usr/bin/sudo
 
 echo "Installed!"
